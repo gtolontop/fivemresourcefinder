235534d203740d624ab44e8a7830eb5d2a22563e
Cruck Store <cruckstore+github@gmail.com>
2025-02-16 09:56:08 +0100

    Enhance NavBar component with active link styling and update SCSS for navigation links

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 const NavBar: React.FC = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -12,11 +14,31 @@ const NavBar: React.FC = () => {
           </Link>
         </div>
         <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/resources">Resources</Link></li>
-          <li><Link to="/store">Store</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/login">Login</Link></li>
+          <li>
+            <Link to="/" className={location.pathname === "/" ? "active-link" : ""}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/resources" className={location.pathname === "/resources" ? "active-link" : ""}>
+              Resources
+            </Link>
+          </li>
+          <li>
+            <Link to="/store" className={location.pathname === "/store" ? "active-link" : ""}>
+              Store
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className={location.pathname === "/about" ? "active-link" : ""}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/login" className={location.pathname === "/login" ? "active-link" : ""}>
+              Login
+            </Link>
+          </li>
         </ul>
       </div>
     </nav>
